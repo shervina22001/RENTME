632127c98dd8079148662b6e11211cc2374d9cf4
shervina22001 <shervina22001@mail.unpad.ac.id>
2023-05-26 18:08:02 +0700

    fix .env definition in connection.php

--- connection.php
+++ connection.php
@@ -1,12 +1,19 @@
 <?php
+  $env = parse_ini_file('.env');
 
-  $dsn = "mysql:host={$_ENV["HOST"]};dbname={$_ENV["DATABASE"]}";
+
+  $host = $env['SISDAT_HOST'];
+  $username = $env['SISDAT_USERNAME'];
+  $password = $env['SISDAT_PASSWORD'];
+  $database = $env['SISDAT_DATABASE'];
+
+  $dsn = "mysql:host={$host};dbname={$database}";
   $options = array(
-    PDO::MYSQL_ATTR_SSL_CA => "ca-certificates.pem",
+    PDO::MYSQL_ATTR_SSL_CA => 'ca-certificates.pem'
   );
 
   // Create a PDO instance representing a connection to a database
-  $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
+  $pdo = new PDO($dsn, $username, $password, $options);
 
   // Set the PDO error mode to exception
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
